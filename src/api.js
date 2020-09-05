f34e198af7f671a2f5a96a6c5c65ff67e0550f37
BlakeAllred <blake.allred95@icloud.com>
2020-09-04 22:58:43 -0600

    Proper mapped rendering

--- src/api.js
+++ src/api.js
@@ -5,29 +5,40 @@ export default class API extends Component {
        super(props)
 
        this.state = {
-           data: ''
+           loaded: false,
+           data: []
        }
+
+       
    }
 
    componentDidMount() {
-    fetch('https://services2.arcgis.com/sJvSsHKKEOKRemAr/arcgis/rest/services/Bigfoot%20Locations/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json')
-    .then((response) => {
-      return response.json();
-    })
-    .then((myJson) => {
-      this.setState({data: myJson.features[0].attributes})
-      console.log(this.state.data)
-      
-    });
-   }
+    fetch(
+      "https://services2.arcgis.com/sJvSsHKKEOKRemAr/arcgis/rest/services/Bigfoot%20Locations/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((myJson) => {
+        this.setState({
+          data: myJson.features,
+          loaded: true,
+        });
+        console.log(this.state.data);
+      });
+  }
 
+  render() {
+    // Data is still loading, display an intermediate message
+    if (!this.state.loaded) {
+      return <p>Loading...</p>;
+    }
+    return <div className="">{this.state.data.map(item=> {
+        return <div><p>{item.attributes.STATE_NAME}</p>
+                    <p>{item.attributes.Year}</p>
+        </div>
+                
+    })}</div>;
+  }
 
-
-render() {
-       return (
-           <div className = ''>
-               {this.state.data.STATE_NAME}
-           </div>
-       )
-   }
 }
